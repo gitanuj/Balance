2ae22815b878fa0f4a74d4270c32cf362e4c1dfd
Tanuj Mittal <free2talk.183@gmail.com>
2011-04-26 02:08:58 +0530

    fixed layout & added images

--- gen/com/tanuj/balance/R.java
+++ gen/com/tanuj/balance/R.java
@@ -16,18 +16,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int about=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int button=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int settings=0x7f020003;
     }
     public static final class id {
-        public static final int b1=0x7f070003;
-        public static final int b2=0x7f070002;
-        public static final int b3=0x7f070004;
-        public static final int m1=0x7f070005;
-        public static final int tv1=0x7f070001;
-        public static final int widget38=0x7f070000;
+        public static final int b1=0x7f070000;
+        public static final int b2=0x7f070001;
+        public static final int b3=0x7f070002;
+        public static final int m1=0x7f070003;
+        public static final int m2=0x7f070004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
@@ -39,7 +41,10 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int min=0x7f040007;
         public static final int pre_balance=0x7f040006;
+        public static final int settings=0x7f040008;
         public static final int sms=0x7f040005;
+        public static final int taptochange=0x7f040009;
         public static final int title=0x7f040004;
+        public static final int ussd=0x7f04000a;
     }
 }
--- src/com/tanuj/balance/Main.java
+++ src/com/tanuj/balance/Main.java
@@ -2,8 +2,10 @@ package com.tanuj.balance;
 
 import android.app.Activity; 
 import android.content.Intent; 
+import android.content.SharedPreferences;
 import android.net.Uri; 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,6 +19,10 @@ public class Main extends Activity implements OnClickListener
     private Button b1;
     private Button b2;
     private Button b3;
+    public static String s1 = "*141#";
+    public static String s2 = "*157#";
+    public static String s3 = "*156#";
+    public static boolean s;
     
     /** Called when the activity is first created. */ 
     @Override 
@@ -35,18 +41,74 @@ public class Main extends Activity implements OnClickListener
         b3.setOnClickListener(this); 
     } 
 
-	public void onClick(View v) 
+	@Override
+	protected void onResume() 
 	{
-		String val = "";
-		String encodedHash = Uri.encode("#"); 
+		super.onResume();
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		s = preferences.getBoolean("button", false);
+		
+		if(s == true)
+		{
+			b1.setBackgroundDrawable(getResources().getDrawable(R.drawable.button));
+			b2.setBackgroundDrawable(getResources().getDrawable(R.drawable.button));
+			b3.setBackgroundDrawable(getResources().getDrawable(R.drawable.button));
+		}
+		else
+		{
+			b1.setBackgroundDrawable(null);
+			b2.setBackgroundDrawable(null);
+			b3.setBackgroundDrawable(null);
+			b1.setBackgroundColor(android.graphics.Color.GRAY);
+			b2.setBackgroundColor(android.graphics.Color.GRAY);
+			b3.setBackgroundColor(android.graphics.Color.GRAY);
+		}
+	}
+
+	public void onClick(View v) 
+	{	
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		s1 = preferences.getString("etp1", s1);
+		s2 = preferences.getString("etp2", s2);
+		s3 = preferences.getString("etp3", s3);
+		String val = "", ss[];
+		String encodedHash = Uri.encode("#");
 		String tag = (String)v.getTag();
 		if(tag.equals("b1"))
-			val = "*141";
+		{
+			ss = s1.split("#");
+			for(int i=0 ; i<ss.length ; i++)
+			{
+				val += ss[i];
+				val += encodedHash;
+			}
+			if(s1.charAt(s1.length()-1) != '#')
+				val = val.substring(0, val.length()-1-encodedHash.length());
+		}
 		else if(tag.equals("b2"))
-			val = "*157";
+		{
+			ss = s2.split("#");
+			for(int i=0 ; i<ss.length ; i++)
+			{
+				val += ss[i];
+				val += encodedHash;
+			}
+			if(s2.charAt(s2.length()-1) != '#')
+				val = val.substring(0, val.length()-1-encodedHash.length());
+		}
 		else if(tag.equals("b3"))
-			val = "*156";
-		call(val+encodedHash);
+		{
+			 ss = s3.split("#");
+			for(int i=0 ; i<ss.length ; i++)
+			{
+				val += ss[i];
+				val += encodedHash;
+			}
+			if(s3.charAt(s3.length()-1) != '#')
+				val = val.substring(0, val.length()-1-encodedHash.length());
+		}
+		call(val);
 	} 
 	
 	protected void call(String phoneNumber)
@@ -73,8 +135,20 @@ public class Main extends Activity implements OnClickListener
 	public boolean onOptionsItemSelected(MenuItem item) 
 	{
 	    // Handle item selection
-		Intent intent = new Intent("org.openintents.action.SHOW_ABOUT_DIALOG");
-		startActivityForResult(intent, 0);
+		String title = (String)item.getTitle();
+		Intent intent;
+		
+		if(title.equals("About"))
+		{
+			intent = new Intent("org.openintents.action.SHOW_ABOUT_DIALOG");
+			startActivityForResult(intent, 0);
+		}
+		else
+		{
+			intent = new Intent(getBaseContext(), Settings.class);
+			startActivity(intent);
+		}
+		
 		return true;
 	}
 }
